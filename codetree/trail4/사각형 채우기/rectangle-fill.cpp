f187d2049e8b2e8448a0526ffcb16038392fa090
DOHOON <kkdh1215@gmail.com>
2026-07-07 13:55:17 +0900

    [Passed] 사각형 채우기 (2ms, 0MB)

---
+++ codetree/trail4/사각형 채우기/rectangle-fill.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int n;
+int memo[1004];
+
+int square(int cnt) {
+    if(memo[cnt] != -1) {
+        return memo[cnt];
+    }
+
+    if(cnt == 1) {
+        memo[cnt] = 1;
+    }
+    else if (cnt == 2) {
+        memo[cnt] = 2;
+    }
+    else {
+        memo[cnt] = (square(cnt-1) + square(cnt-2)) % 10007;
+    }
+
+    return memo[cnt];
+
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+
+    memset(memo, -1, sizeof(memo));
+
+    square(n);
+
+    cout << memo[n];
+
+    return 0;
+}
