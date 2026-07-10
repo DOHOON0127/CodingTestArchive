753fa352a8305292b09a81c0447a2db7e25a25c0
DOHOON <kkdh1215@gmail.com>
2026-07-10 17:18:50 +0900

    [Passed] 문자열 정렬 (1ms, 0MB)

---
+++ codetree/trail2/문자열 정렬/string-sort.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+string s;
+
+int main() {
+
+    cin >> s;
+
+    sort(s.begin(), s.end());
+
+    cout << s;
+
+    return 0;
+}
