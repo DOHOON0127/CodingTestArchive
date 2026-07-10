c1e7606c7a529573114f2ee54818d9cb9f7bca78
DOHOON <kkdh1215@gmail.com>
2026-07-10 23:21:10 +0900

    [Passed] 단어 정렬 (2ms, 0MB)

---
+++ codetree/trail2/단어 정렬/sorting-words.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n;
+string arr[101];
+
+int main() {
+
+    cin >> n;
+
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    sort(arr, arr+n);
+
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << '\n';
+    }
+
+    return 0;
+}
