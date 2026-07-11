4e25af2fab754d1e608e54f9d2357e75ed03bd53
DOHOON <kkdh1215@gmail.com>
2026-07-11 17:29:53 +0900

    [Passed] k번째로 신기한 문자열 (2ms, 0MB)

--- codetree/trail2/k번째로 신기한 문자열/kth-special-string.cpp
+++ codetree/trail2/k번째로 신기한 문자열/kth-special-string.cpp
@@ -15,25 +15,21 @@ int main() {
         cin >> arr[i];
     }
 
-
-    sort(arr, arr+n);
-
     vector<string> V;
     
     for(int i = 0; i < n; i++) {
         bool isMatch = true;
-        
-        for(int j = 0; j < (int)t.size(); j++) {
-            if(arr[i][j] != t[j]) {
-                isMatch = false;
-            }
+
+        if (arr[i].size() < t.size()) {
+            continue;
         }
 
-        if(isMatch) {
+        if(arr[i].substr(0, t.size()) == t) {
             V.push_back(arr[i]);
         }
-
     }
+
+    sort(V.begin(), V.end());
 
     cout << V[k-1];
 
