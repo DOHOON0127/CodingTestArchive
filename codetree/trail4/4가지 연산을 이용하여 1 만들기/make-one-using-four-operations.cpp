01b4f606cd1a8169fe2e4767165baa45eebea17e
DOHOON <kkdh1215@gmail.com>
2026-07-12 13:53:09 +0900

    [Passed] 4가지 연산을 이용하여 1 만들기 (2ms, 0MB)

--- codetree/trail4/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.cpp
+++ codetree/trail4/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.cpp
@@ -2,57 +2,49 @@
 #include <queue>
 using namespace std;
 
+const int MAX_LIMIT = 1000000;
+
 int N;
-bool visited[1000004];
-int arr[1000004];
-int min_num[1000004];
+bool visited[MAX_LIMIT+4];
+int arr[MAX_LIMIT+4];
+int min_num[MAX_LIMIT+4];
+
+queue<int> Q;
+
+void push_node(int next_num, int curr_num) {
+    if(next_num >=1 && next_num <= MAX_LIMIT && visited[next_num] == false) {
+        Q.push(next_num);
+        visited[next_num] = true;
+        min_num[next_num] = curr_num + 1;
+    }
+}
 
 void BFS(int n) {
-    queue<int> Q;
     Q.push(n);
     visited[n] = true;
-
-    if(n == 1) {
-        cout << 0;
-        return;
-    }
 
     while(!Q.empty()) {
         int curr_num = Q.front();
         Q.pop();
 
-        if(visited[curr_num+1] == false) {
-            Q.push(curr_num+1);
-            visited[curr_num+1] = true;
-            min_num[curr_num+1] = min_num[curr_num] + 1;
+
+        if(curr_num == 1) {
+            cout <<  min_num[1];
+            return;
         }
 
-        if(visited[curr_num-1] == false) {
-            Q.push(curr_num-1);
-            visited[curr_num-1] = true;
-            min_num[curr_num-1] = min_num[curr_num] + 1;        
-        }
+
+        push_node(curr_num+1, min_num[curr_num]);
+        push_node(curr_num-1, min_num[curr_num]);
 
         if(curr_num % 2 == 0) {
-            if(visited[curr_num/2] == false) {
-                Q.push(curr_num/2);
-                visited[curr_num/2] = true;
-                min_num[curr_num/2] = min_num[curr_num] + 1;        
-            }
+            push_node(curr_num / 2, min_num[curr_num]);
         } 
 
         if(curr_num % 3== 0) {
-            if(visited[curr_num/3] == false) {
-                Q.push(curr_num/3);
-                visited[curr_num/3] = true;
-                min_num[curr_num/3] = min_num[curr_num] + 1;        
-            }
+            push_node(curr_num / 3, min_num[curr_num]);
         }
-
-        if(min_num[1] != 0) {
-            cout << min_num[1];
-            return;
-        }     
+    
     }
 }
 
