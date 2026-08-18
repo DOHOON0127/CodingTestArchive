8fc9ab66ed40e63fc726dce37a50dc4f2d3898a0
DOHOON <kkdh1215@gmail.com>
2026-08-18 18:23:22 +0900

    [Passed] 문자열의 길이 출력하기 (2ms, 0MB)

---
+++ codetree/trail1/문자열의 길이 출력하기/print-strings-length.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    string str1;
+    string str2;
+
+    cin >> str1 >> str2;
+
+    cout << str1.size() + str2.size();
+
+    return 0;
+}
